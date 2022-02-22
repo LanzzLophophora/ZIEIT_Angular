cf93db72a1c75e8469d1fd67cac594648d5ce401
lanzzLophophora <lanzz77781@gmail.com>
2022-02-22 15:51:50 +0200

    lr 7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {PostsComponent} from "./posts/posts.component";
+import { PostComponent } from "./post/post.component";
+import { AboutExtraComponent } from "./about-extra/about-extra.component";
 
-const routes: Routes = [];
-
+const router: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent, children: [
+      {path: 'extra', component: AboutExtraComponent}]
+  },
+  {path: 'posts', component: PostsComponent },
+  {path: 'post/:id', component: PostComponent }
+ ]
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports:[RouterModule.forRoot(router)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
